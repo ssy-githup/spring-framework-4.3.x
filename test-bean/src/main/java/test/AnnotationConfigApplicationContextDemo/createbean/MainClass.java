0656afb8d14f338dd780d9740236d43ea66f5ce5
897645431@qq.com <897645431@qq.com>
2020-07-12 22:17:08 +0800

    *调用bean工厂的后置处理器

--- test-bean/src/main/java/test/AnnotationConfigApplicationContextDemo/createbean/MainClass.java
+++ test-bean/src/main/java/test/AnnotationConfigApplicationContextDemo/createbean/MainClass.java
@@ -4,11 +4,12 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 /**
  * 创建相互依赖的bean
+ * spring 是如何解决循环依赖的
  */
 public class MainClass {
 
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(MainConfig.class);
-        System.out.println(context.getBean("MyAspect"));
+        System.out.println(context.getBean("Person"));
     }
 }
